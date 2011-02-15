d6c4a11ed807ae6d8ffd93e0943d2f5af405b545
Andrew Cholakian <andrew@andrewvc.com>
2011-02-14 18:48:17 -0800

    Fixed channel specs

--- spec/messages/channel_message_spec.rb
+++ spec/messages/channel_message_spec.rb
@@ -31,8 +31,9 @@ describe SocketMotor::ChannelMessage do
         @sub_message = SocketMotor::ChannelMessage::Control.new(@chan_name,'subscribe',@conn_id)
       end
       it "should set the message contents correctly" do
-        @sub_message.to_hash['name'].should == 'subscribe'
+        @sub_message.to_hash['name'].should == 'control'
         @sub_message.to_hash['body'].should == {'channel_name'  => @chan_name,
+                                                'operation'     => 'subscribe',
                                                 'connection_id' => @conn_id}
       end
       it "should recreate properly" do
@@ -45,9 +46,10 @@ describe SocketMotor::ChannelMessage do
         @unsub_message = SocketMotor::ChannelMessage::Control.new(@chan_name,'unsubscribe',@conn_id)
       end
       it "should set the message contents correctly" do
-        @unsub_message.to_hash['name'].should == 'unsubscribe'
+        @unsub_message.to_hash['name'].should == 'control'
         @unsub_message.to_hash['body'].should == {'channel_name'  => @chan_name,
-                                                'connection_id' => @conn_id}
+                                                  'operation'     => 'unsubscribe',
+                                                  'connection_id' => @conn_id}
       end
       it "should recreate properly" do
         recreated = SocketMotor::ChannelMessage::Control.from_hash(@unsub_message.to_hash)
